7082731c66c003508ab8124acc5728591bec0f3b
bmoral64 <abrianmorales64@gmail.com>
2022-07-01 13:45:17 -0400

    Update PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -9,12 +9,10 @@ password[0] = chr(random.randint(65,90))
 password[1] = chr(random.randint(65,90))
 
 # lowercase letters
-
 password[2] = chr(random.randint(97,122))
 password[3] = chr(random.randint(97,122))
 
 # Digits
-
 password[4] = chr(random.randint(48,57))
 password[5] = chr(random.randint(48,57))
 
@@ -22,7 +20,6 @@ password[5] = chr(random.randint(48,57))
 password[6] = chr(random.randint(33,95))
 password[7] = chr(random.randint(33,95))
 
-# Shuffle the password
 random.shuffle(password)
 for x in password:
     print(x, end='')
